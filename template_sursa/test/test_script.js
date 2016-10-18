74fb47263a2380b534ceb9b308f069215d0227b5
cristianbanarescu <cristianbanarescu@gmail.com>
2016-10-18 16:21:56 +0300

    modalv2

--- template_sursa/test/test_script.js
+++ template_sursa/test/test_script.js
@@ -1,9 +1,9 @@
-function openModalRM() {
-  document.getElementById('myModalRM').style.display = "block";
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
   }
 
-function closeModalRM() {
-  document.getElementById('myModalRM').style.display = "none";
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
 }
 
 var slideIndex = 1;
@@ -19,82 +19,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlidesRM");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-function openModalB() {
-  document.getElementById('myModalB').style.display = "block";
-  }
-
-function closeModalB() {
-  document.getElementById('myModalB').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlidesB");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-
-function openModalMU() {
-  document.getElementById('myModalMU').style.display = "block";
-  }
-
-function closeModalMU() {
-  document.getElementById('myModalMU').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlidesMU");
+  var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
