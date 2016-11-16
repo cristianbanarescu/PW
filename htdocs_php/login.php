7b5ef8e28bb0243c3181bded57032e01b68cb652
cipryd <cipri.dunca@gmail.com>
2016-11-16 16:26:03 +0200

    login

---
+++ htdocs_php/login.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	
+</head>
+
+<body>
+	<form method="post" action="index_site2.php">
+		Username : <input type="text" name="username"><br>
+		Password : <input type="text" name="password"><br>
+		<input type="submit"  name ="form_create">
+	</form>
+</body>
+</html>
